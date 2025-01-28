e47f672a45537c99b55f7475a1e89cadecfe1652
Jane Fan <jfan9988@gmail.com>
2025-01-28 15:50:36 +1100

    basic drag logic

--- src/lib/components/frame.svelte
+++ src/lib/components/frame.svelte
@@ -1,6 +1,19 @@
 <script lang="ts">
+  export let aspectRatio = '4 / 5';
+
   let imgSrc = '';
   let files: FileList | undefined = undefined;
+  let frameRef: HTMLDivElement | undefined = undefined;
+
+  $: frameWidth = frameRef?.clientWidth || 1;
+  let isDragging = false;
+  let startPosX = 0;
+  let startPosY = 0;
+  let posX = 0;
+  let posY = 0;
+  $: movePercentX = (100 * (startPosX - posX)) / frameWidth;
+  $: movePercentY = (100 * (startPosY - posY)) / frameWidth;
+
   $: hasFile = files && files.length;
 
   // This effect is called upon image upload
@@ -16,9 +29,58 @@
     imgSrc = '';
     files = undefined;
   };
+
+  const handleMouseStart = (e: MouseEvent) => {
+    e.preventDefault();
+    startPosX = e.pageX;
+    startPosY = e.pageY;
+    isDragging = true;
+  };
+
+  const handleTouchStart = (e: TouchEvent) => {
+    e.preventDefault();
+    startPosX = e.touches[0].pageX;
+    startPosY = e.touches[0].pageY;
+    isDragging = true;
+  };
+
+  const handleMouseMove = (e: MouseEvent) => {
+    e.preventDefault();
+    if (!isDragging) {
+      return;
+    }
+    posX = e.pageX;
+    posY = e.pageY;
+  };
+
+  const handleTouchMove = (e: TouchEvent) => {
+    e.preventDefault();
+    if (!isDragging) {
+      return;
+    }
+    posX = e.touches[0].pageX;
+    posY = e.touches[0].pageY;
+  };
+
+  const handleMoveEnd = () => {
+    isDragging = false;
+  };
 </script>
 
-<div class="frame" class:has-border={!hasFile} style="background-image: url({imgSrc});">
+<p>{movePercentX} {movePercentY}</p>
+<!-- svelte-ignore a11y-no-static-element-interactions -->
+<div
+  class="frame"
+  class:has-border={!hasFile}
+  style="background-image: url({imgSrc}); aspect-ratio: {aspectRatio}; background-position: {movePercentX}% {movePercentY}%;"
+  bind:this={frameRef}
+  on:mousedown={handleMouseStart}
+  on:touchstart={handleTouchStart}
+  on:mousemove={handleMouseMove}
+  on:touchmove={handleTouchMove}
+  on:mouseup={handleMoveEnd}
+  on:touchend={handleMoveEnd}
+>
   <div class="overlay">
     {#if hasFile}
       <button class="remove-button" type="button" on:click={removeFile}>X</button>
@@ -37,12 +99,11 @@
 
 <style>
   .frame {
-    aspect-ratio: 4 / 5;
     position: relative;
     border-radius: 1rem;
     overflow: hidden;
     background-size: cover;
-    background-position: center;
+    background-repeat: no-repeat;
   }
 
   .has-border {
